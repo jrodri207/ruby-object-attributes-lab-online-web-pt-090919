bd5efcd807afa25dc2cb0cc7f922dc460ae73dc2
Joshua Rodriguez <josh030596@gmail.com>
2019-10-23 00:53:21 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -8,4 +8,13 @@ class Dog
   def name=(new_name)
     @name = new_name
   end 
+  def intialze(breed)
+    @breed = breed 
+  end 
+  def breed 
+    @breed 
+  end 
+  def breed=(new_breed)
+    @breed = new_breed 
+  end 
 end 
